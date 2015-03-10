f08114719aae790952182923b9a2d63db0d858a7
ravjotsingh9 <ravjotsingh9@yahoo.com>
2015-03-09 23:40:00 -0700

    version0.1

---
+++ v0.1/Client/client.go
@@ -0,0 +1,43 @@
+/***
+*	File Name: client.go
+*	Description: The client provides the two functionalities:
+*				 - request move
+*				 - request fire
+*	Date: 	8 March 2015
+*	Author: Glen & Ravjot
+*/
+package main
+import(
+	"fmt"
+)
+
+
+/***
+*	Function Name: 	main()
+*	Desc:			The main function for client
+*	Pre-cond:		
+*	Post-cond:		Call the request functions
+*/
+
+func main(){
+}
+
+
+/***
+*	Function Name: 	move()
+*	Desc:			The function requests move
+*	Pre-cond:		takes connection argument and the new location
+*	Post-cond:		retuen success or return failure
+*/
+func move(){
+}
+
+
+/***
+*	Function Name: 	fire()
+*	Desc:			The function requests fire
+*	Pre-cond:		takes connection argument and name of client who is fired
+*	Post-cond:		return success or returns failure
+*/
+func fire(){
+}
---
+++ v0.1/Server/server.go
@@ -0,0 +1,43 @@
+/***
+*	File Name: server.go
+*	Description: The server provides the two functionalities:
+*				 - service move request
+*				 - service fire request
+*	Date: 	8 March 2015
+*	Author: Glen & Ravjot
+*/
+package main
+import(
+	"fmt"
+)
+
+
+/***
+*	Function Name: 	main()
+*	Desc:			The main function for server
+*	Pre-cond:		
+*	Post-cond:		Call the service functions
+*/
+
+func main(){
+}
+
+
+/***
+*	Function Name: 	serviceMoveReq()
+*	Desc:			The function provide service to client's move request
+*	Pre-cond:		takes connection argument and the new location
+*	Post-cond:		list is updated with new location or return failure
+*/
+func serviceMoveReq(){
+}
+
+
+/***
+*	Function Name: 	serviceFireReq()
+*	Desc:			The function provide service to client's fire request
+*	Pre-cond:		takes connection argument and name of client who is fired
+*	Post-cond:		Destroy the client or returns failure
+*/
+func serviceFireReq(){
+}
